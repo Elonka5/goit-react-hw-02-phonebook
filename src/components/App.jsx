a779cc2f024f8601caff895b45690dab80202341
Elonka5 <101475428+Elonka5@users.noreply.github.com>
2023-08-24 13:26:41 +0300

    add hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Form } from './Form/Form';
+import { Section } from './Section/Section';
+import { ContactsList } from './ContactsList/ContactsList';
+import { Filter } from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  handleSubmit = idInputName => {
+    const isExist = this.state.contacts.find(
+      contact => contact.name.toLowerCase() === idInputName.name.toLowerCase()
+    );
+    if (isExist) {
+      alert(`${idInputName.name} is already in contacts.`);
+      return;
+    }
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, idInputName],
+    }));
+    console.log(idInputName);
+  };
+
+  handleDeleteContact = idContact => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== idContact),
+    }));
+  };
+
+  handleInputFilter = evt => {
+    const { name, value } = evt.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    const filteredContacts = this.state.contacts.filter(contact =>
+      contact.name
+        .toLowerCase()
+        .includes(this.state.filter.toLowerCase().trim())
+    );
+    return (
+      <div className="container">
+        <Section title="Phonebook">
+          <Form onSubmit={this.handleSubmit}></Form>
+        </Section>
+        <Section title="Contacts">
+          <Filter filterContact={this.handleInputFilter} />
+          <ContactsList
+            contacts={filteredContacts}
+            onRemoveContact={this.handleDeleteContact}
+          ></ContactsList>
+        </Section>
+      </div>
+    );
+  }
+}
